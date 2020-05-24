706dc362953516c9dcfee1da0ddbe39c5a13d300
Shadab Gada <shadabgada234@gmail.com>
2020-05-24 14:25:18 +0530

    make it better

--- app.py
+++ app.py
@@ -15,64 +15,66 @@ app = Flask(__name__)
 @cross_origin()# It allows requests comes from different machine all over the world. cloud block those requests.
 def index():
     if request.method == 'POST':
-        searchString = request.form['content']
         try:
-            dbConn = pymongo.MongoClient("mongodb://localhost:27017/")  # opening a connection to Mongo
-            db = dbConn['crawlerDB']
-            reviews = db[searchString].find({})
-            if reviews.count() > 0:
-                return render_template('results.html',reviews=reviews)
-            else:
-                flipkart_url = "https://www.flipkart.com/search?q=" + searchString
-                uClient = uReq(flipkart_url)
-                flipkartPage = uClient.read()
-                uClient.close()
-                flipkart_html = bs(flipkartPage, "html.parser")
-                bigboxes = flipkart_html.findAll("div", {"class": "bhgxx2 col-12-12"})
-                del bigboxes[0:3]
-                box = bigboxes[0]
-                productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
-                prodRes = requests.get(productLink)
-                prod_html = bs(prodRes.text, "html.parser")
-                commentboxes = prod_html.find_all('div', {'class': "_3nrCtb"})
+            searchString = request.form['content'].replace(" ", "")
+            flipkart_url = "https://www.flipkart.com/search?q=" + searchString
+            uClient = uReq(flipkart_url)
+            flipkartPage = uClient.read()
+            uClient.close()
+            flipkart_html = bs(flipkartPage, "html.parser")
+            bigboxes = flipkart_html.findAll("div", {"class": "bhgxx2 col-12-12"})
+            del bigboxes[0:3]
+            box = bigboxes[0]
+            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+            prodRes = requests.get(productLink)
+            prodRes.encoding = 'utf-8'
+            prod_html = bs(prodRes.text, "html.parser")
+            # print(prod_html)
+            commentboxes = prod_html.find_all('div', {'class': "_3nrCtb"})
 
-                table = db[searchString]
-                filename = searchString+".csv"
-                fw = open(filename, "w")
-                headers = "Product, Customer Name, Rating, Heading, Comment \n"
-                fw.write(headers)
-                reviews = []
-                for commentbox in commentboxes:
-                    try:
-                        name = commentbox.div.div.find_all('p', {'class': '_3LYOAd _3sxSiS'})[0].text
+            filename = searchString + ".csv"
+            fw = open(filename, "w")
+            headers = "Product, Customer Name, Rating, Heading, Comment \n"
+            fw.write(headers)
+            reviews = []
+            for commentbox in commentboxes:
+                try:
+                    # name.encode(encoding='utf-8')
+                    name = commentbox.div.div.find_all('p', {'class': '_3LYOAd _3sxSiS'})[0].text
 
-                    except:
-                        name = 'No Name'
+                except:
+                    name = 'No Name'
 
-                    try:
-                        rating = commentbox.div.div.div.div.text
+                try:
+                    # rating.encode(encoding='utf-8')
+                    rating = commentbox.div.div.div.div.text
 
-                    except:
-                        rating = 'No Rating'
 
-                    try:
-                        commentHead = commentbox.div.div.div.p.text
-                    except:
-                        commentHead = 'No Comment Heading'
-                    try:
-                        comtag = commentbox.div.div.find_all('div', {'class': ''})
-                        custComment = comtag[0].div.text
-                    except:
-                        custComment = 'No Customer Comment'
-                    fw.write(searchString+","+name.replace(",", ":")+","+rating + "," + commentHead.replace(",", ":") + "," + custComment.replace(",", ":") + "\n")
-                    mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
-                              "Comment": custComment}
-                    x = table.insert_one(mydict)
-                    reviews.append(mydict)
-                return render_template('results.html', reviews=reviews)
-        except:
+                except:
+                    rating = 'No Rating'
+
+                try:
+                    # commentHead.encode(encoding='utf-8')
+                    commentHead = commentbox.div.div.div.p.text
+
+                except:
+                    commentHead = 'No Comment Heading'
+                try:
+                    comtag = commentbox.div.div.find_all('div', {'class': ''})
+                    # custComment.encode(encoding='utf-8')
+                    custComment = comtag[0].div.text
+                except Exception as e:
+                    print("Exception while creating dictionary: ", e)
+
+                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
+                          "Comment": custComment}
+
+                reviews.append(mydict)
+            return render_template('results.html', reviews=reviews[0:(len(reviews) - 1)])
+        except Exception as e:
+            print('The Exception message is: ', e)
             return 'something is wrong'
-            #return render_template('results.html')
+    # return render_template('results.html')
     else:
         return render_template('index.html')
 if __name__ == "__main__":
